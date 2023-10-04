0634250ff6bdd0d30ea38200c93a58780e546509
arxs-abd <70242781+arxs-abd@users.noreply.github.com>
2023-10-04 16:15:07 +0800

    add helper generateRoom

--- utils/helper.js
+++ utils/helper.js
@@ -14,5 +14,16 @@ const createChatAndTime = (message) => {
 
   return result
 }
+function generateRandomString(length) {
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+  let result = ''
+  const charactersLength = characters.length
 
-module.exports = { createChatAndTime }
+  for (let i = 0; i < length; i++) {
+      const randomIndex = Math.floor(Math.random() * charactersLength)
+      result += characters.charAt(randomIndex)
+  }
+
+  return result
+}
+module.exports = { createChatAndTime, generateRandomString }
